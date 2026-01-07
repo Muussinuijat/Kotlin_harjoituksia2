54323e8e6985180aa367adce516ca86f2db191af
Muussinuijat <t3lihe00@students.oamk.fi>
2026-01-07 13:15:18 +0200

    Viikkotehtävä 6 valmis

---
+++ Todo2/app/src/main/java/com/example/todo2/ui/theme/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.todo2.ui.theme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.graphics.Color
+import com.example.todo2.viewmodel.TodoViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.material3.Divider
+import com.example.todo2.model.Todo
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Todo2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TodoScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TodoScreen(modifier: Modifier = Modifier,todoViewModel: TodoViewModel = viewModel()) {
+    TodoList( todos = todoViewModel.todos)
+}
+
+@Composable
+fun TodoList(todos: List<Todo>) {
+    LazyColumn(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        items(todos) { todo->
+            Text(
+                text = todo.title,
+                modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
+            )
+            Divider(color = Color.LightGray, thickness = 1.dp)
+        }
+    }
+}
